d75a0097103206d2843fa0fa2dd2fdf7f60ce283
100156994 <100156994@qq.com>
2018-01-07 11:13:21 +0800

    把数据过滤部分分离出来

--- durAnalysis.py
+++ durAnalysis.py
@@ -10,32 +10,6 @@ import numpy as np
 from pylab import *
 mpl.rcParams['font.sans-serif'] = ['SimHei']
 
-def is_outlier(points, threshold=15):
-    """
-    返回一个布尔型的数组，如果数据点是异常值返回True，反之，返回False。
-
-    数据点的值不在阈值范围内将被定义为异常值
-    阈值默认为15
-    """
-    # 转化为向量
-    if len(points.shape) == 1:
-        points = points[:,None]
-
-    # 数组的中位数
-    median = np.median(points, axis=0)
-
-    # 计算方差
-    diff = np.sum((points - median)**2, axis=-1)
-    #标准差
-    diff = np.sqrt(diff)
-    # 中位数绝对偏差
-    med_abs_deviation = np.median(diff)
-
-
-    modified_z_score = 0.6745 * diff / med_abs_deviation
-
-    # return a mask for each outlier
-    return modified_z_score > threshold
 
 
 #绘制直方图
