ac82972fe20260283396ca738aa22f3144a65339
100156994 <100156994@qq.com>
2018-01-07 11:14:10 +0800

    数据过滤

---
+++ filter.py
@@ -0,0 +1,28 @@
+import numpy as np
+
+def is_outlier(points, threshold=15):
+    """
+    返回一个布尔型的数组，如果数据点是异常值返回True，反之，返回False。
+
+    数据点的值不在阈值范围内将被定义为异常值
+    阈值默认为15
+    """
+    # 转化为向量
+    if len(points.shape) == 1:
+        points = points[:,None]
+
+    # 数组的中位数
+    median = np.median(points, axis=0)
+
+    # 计算方差
+    diff = np.sum((points - median)**2, axis=-1)
+    #标准差
+    diff = np.sqrt(diff)
+    # 中位数绝对偏差
+    med_abs_deviation = np.median(diff)
+
+
+    modified_z_score = 0.6745 * diff / med_abs_deviation
+
+    # return a mask for each outlier
+    return modified_z_score > threshold
